a99e2821eadbc99f84237a815b205f6c38aa69f8
marcelldac <dacmarcell@gmail.com>
2023-10-30 16:27:06 -0300

    feat: use user router

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 import express from "express";
 import cors from "express";
 
+import userRouter from "./routers/users-route";
+
 const PORT = process.env.PORT || 4000;
-
 const HOSTNAME = process.env.HOSTNAME || "http://localhost";
-
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("chupa cu");
-});
 
 app.use(cors());
 
-app.use((req, res) => {
-  res.status(404);
-});
+app.use("/api", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Running on ${HOSTNAME}:${PORT} `);
