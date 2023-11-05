53bc01509d6ff585a5a6c71dc76f41af1ba14748
marcelldac <dacmarcell@gmail.com>
2023-11-05 11:33:56 -0300

    feat: create login route

---
+++ src/routers/login-routes.ts
@@ -0,0 +1,10 @@
+import express, { Router } from "express";
+import { login } from "../controllers/login-controller";
+
+const loginRouter: Router = express.Router();
+
+loginRouter.use(express.json());
+
+loginRouter.post("/login", login);
+
+export default loginRouter;
