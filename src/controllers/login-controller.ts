25100768ea4eb285b50990612203e5d5369fb3b0
marcelldac <dacmarcell@gmail.com>
2023-11-10 19:35:40 -0300

    feat: add try/catch to authorization, add docs and types to verification

--- src/controllers/login-controller.ts
+++ src/controllers/login-controller.ts
@@ -18,55 +18,71 @@ const prisma: PrismaClient = new PrismaClient();
  * Express.js framework. It represents the HTTP response that will be sent back to the client.
  * @returns a response with a status code of 201 and a JSON object containing a token.
  */
-const authorization = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
+const authorization = async (req: Request, res: Response) => {
   //TODO: Resolver bug de geração de token mesmo se estiver com a senha errada.
   const { email, password }: LoginModel = req.body;
 
-  const userExists = await prisma.user.findUnique({
-    where: { email },
-  });
+  try {
+    const userExists = await prisma.user.findUnique({
+      where: { email },
+    });
 
-  if (!userExists) {
-    return res.status(401).json({ message: "Invalid credentials" });
+    if (!userExists) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordValid = compare(password, userExists.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const jwtToken = jwt.sign({ sub: userExists.id }, process.env.JWT_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    return res.status(201).json({ token: jwtToken });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  } finally {
+    await prisma.$disconnect;
   }
-
-  const isPasswordValid = compare(password, userExists.password);
-
-  if (!isPasswordValid) {
-    return res.status(401).json({ message: "Invalid credentials" });
-  }
-
-  const jwtToken = jwt.sign({ sub: userExists.id }, process.env.JWT_SECRET!, {
-    expiresIn: "1d",
-  });
-
-  return res.status(201).json({ token: jwtToken });
 };
 //#endregion
 
+//#region Login Verification
+/**
+ * The verification function checks if an authorization token is provided in the request headers,
+ * verifies the token using a JWT secret, and calls the next middleware function if the token is valid.
+ * @param {Request} req - The `req` parameter represents the HTTP request object, which contains
+ * information about the incoming request such as headers, query parameters, and request body.
+ * @param {Response} res - The `res` parameter is an object representing the HTTP response that will be
+ * sent back to the client. It is used to send the response data, such as the status code and the
+ * response body.
+ * @param {NextFunction} next - The `next` parameter is a function that is used to pass control to the
+ * next middleware function in the request-response cycle. It is typically called at the end of the
+ * current middleware function to indicate that it has completed its processing and the next middleware
+ * function should be called.
+ * @returns a response with a status code and a JSON object containing a message.
+ */
 const verification = (req: Request, res: Response, next: NextFunction) => {
-  const authToken = req.headers["authorization"];
+  const authToken: string | undefined = req.headers["authorization"];
 
   if (!authToken) {
-    return res.status(401).json({ message: "Token does not provided." });
+    return res.status(401).json({ error: "Token does not provided." });
   }
 
-  const token = authToken?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Invalid token." });
-  }
+  const token = authToken.split(" ")[1];
 
   try {
     jwt.verify(token, process.env.JWT_SECRET!);
     next();
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error });
+    res.status(500).json({ error });
   }
 };
+//#endregion
 
 export { authorization, verification };
